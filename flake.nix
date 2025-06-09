6c913e9f5fdd7d78ed88691b23c800c6b6c9a809
John Lambert <john@example.com>
2025-06-09 19:18:12 -0400

    Initial commit!

---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  description = "";
+  # Each flake is just a set of inputs, a set of outputs and an optional description. The inputs in this basic example will be Nix, home-manager, and snowfall. 
+  inputs = {
+    # NixPkgs (nixos-24.05)
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+
+    # NixPkgs Unstable (nixos-unstable)
+    #unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    ### Additional Inputs ###
+
+
+    # Home Manager (release-24.05)
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # Snowfall Lib
+    snowfall-lib = {
+      url = "github:snowfallorg/lib";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+ };
+
+  outputs =
+    inputs:
+    let
+      lib = inputs.snowfall-lib.mkLib {
+        inherit inputs;
+        src = ./.;
+
+        snowfall = {
+          meta = {
+            name = "homelab";
+            title = "NixOS config called homelab";
+          };
+          namespace = "homelab";
+        };
+      };
+    in
+    lib.mkFlake {
+      inherit inputs;
+      src = ./.;
+
+      channels-config = {
+        allowUnfree = true;
+        permittedInsecurePackages = [ ];
+      };
+
+
+   };
+}
---
+++ homes/user@server/default.nix
@@ -0,0 +1,22 @@
+{
+  config,
+  lib,
+  osConfig,
+  namespace,
+  ...
+}:
+with lib;
+with lib.${namespace};
+{
+  snowfallorg.user.enable = true;
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = lib.mkDefault (osConfig.system.stateVersion or "25.05");
+}
