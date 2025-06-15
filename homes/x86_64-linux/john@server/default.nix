65f076c2066362f258374da46c0355a7018af461
John Lambert <john@example.com>
2025-06-15 00:32:55 +0000

    Testing issue with git sync.

--- homes/x86_64-linux/john@server/default.nix
+++ homes/x86_64-linux/john@server/default.nix
@@ -11,4 +11,50 @@ with lib.homelab;
 {
   homelab.fish.enable = true; 
   home.stateVersion = lib.mkDefault (osConfig.system.stateVersion or "24.11");
+  programs.git = {
+    enable = true;
+    userName = "John Lambert";
+    userEmail = "john@example.com";
+    extraConfig = {
+      core.editor = "nvim";
+      pull.rebase = true;
+      init.defaultBranch = "main";
+      alias.lg = "log --oneline --graph --decorate";
+    };
+  };
+
+  programs.ssh = {
+    enable = true;
+    addKeysToAgent = "yes";
+    matchBlocks = {
+      "github.com" = {
+        hostname = "github.com";
+        user = "git";
+        identityFile = "~/id_ed25519";
+      };
+    };
+  };
+
+  services.ssh-agent.enable = true;
+
+  programs.fish = {
+    enable = true;
+    interactiveShellInit = ''
+      echo "Hello from HomeManager."
+      source ~/OPENAI_API_KEY.env
+      # Apply Gruvbox theme
+      set -g fish_color_normal brblack
+      set -g fish_color_command brgreen
+      set -g fish_color_comment brblue
+      set -g fish_color_error brred
+      set -g fish_color_selection --background=brblack
+      set -g fish_color_search_match --background=brblack
+      set -g fish_color_operator brcyan
+      set -g fish_color_escape brmagenta
+      set -g fish_color_param brcyan
+      set -g fish_color_quote bryellow
+      set -g fish_color_redirection brmagenta
+      set -g fish_color_end brmagenta
+    '';
+  };
 }
