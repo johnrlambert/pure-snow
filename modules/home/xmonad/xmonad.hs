b1001a1002e09411d7f392ad72b9e139945e26dc
John Lambert <you@example.com>
2026-03-24 17:06:21 +0000

    feat: add ManageDocks and wrap xmonad config with docks

--- modules/home/xmonad/xmonad.hs
+++ modules/home/xmonad/xmonad.hs
@@ -2,12 +2,13 @@ import XMonad
 import XMonad.Layout.Spiral
 import XMonad.Layout.Spacing (spacingRaw, Border(..), Spacing)
 import XMonad.Layout.LayoutModifier (ModifiedLayout)
+import XMonad.Hooks.ManageDocks
 
 mySpacing :: Integer -> l a -> ModifiedLayout Spacing l a
 mySpacing i = spacingRaw True (Border i i i i) True (Border i i i i) True
 
 main :: IO ()
-main = xmonad def
+main = xmonad $ docks def
   { terminal = "alacritty"
   , modMask = mod4Mask
   , layoutHook = mySpacing 5 $ spiral (6/7)
