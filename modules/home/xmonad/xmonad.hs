c4acffda5ec79c39394f28402a92fc4a8839ae0a
John Lambert <you@example.com>
2026-03-24 16:31:49 +0000

    fix: correct mySpacing type signature

--- modules/home/xmonad/xmonad.hs
+++ modules/home/xmonad/xmonad.hs
@@ -2,7 +2,7 @@ import XMonad
 import XMonad.Layout.Spiral
 import XMonad.Layout.Spacing
 
-mySpacing :: Integer -> l a -> XMonad.Layout.ModifiedLayout Spacing l a
+mySpacing :: Integer -> l a -> XMonad.Layout.Spacing l a
 mySpacing i = spacingRaw True (Border i i i i) True (Border i i i i) True
 
 main :: IO ()
